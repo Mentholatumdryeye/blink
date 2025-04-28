c6c6d5382d18332d0648241d18ade59839e6f218
Mentholatumdryeye <mentholatumdryeye@gmail.com>
2025-04-28 11:48:02 +0800

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,38 +1,76 @@
-var languageScript;
-
-$(function(){
-  // add language file and convert words
-  appendLanguageScript();
-  convertLanguage();
-
-  // test
-	$('body').on('click', '#test_button_ja' , function() {
-    languageScript.remove();
-    lang = "ja";
-    appendLanguageScript();
-		convertLanguage();
-	});
-  
-	$('body').on('click', '#test_button_en' , function() {
-    languageScript.remove();
-    lang = "en";
-    appendLanguageScript();
-		convertLanguage();
-	});
-});
-
-function appendLanguageScript()
-{
-  languageScript = $('<script>').attr({
-    'type': 'text/javascript',
-    'src': './language/'+lang+'.js'
-  });
-  $('body').append(languageScript);
-}
-
-function convertLanguage()
-{
-  for (let key in languageList) {
-    $('.'+key).text(languageList[key]);
-  }
+var languageScript;
+
+
+
+
+$(function(){
+
+  // add language file and convert words
+
+  appendLanguageScript();
+
+  convertLanguage();
+
+
+
+  // test
+
+	$('body').on('click', '#test_button_ja' , function() {
+
+    languageScript.remove();
+
+    lang = "ja";
+
+    appendLanguageScript();
+
+		convertLanguage();
+
+	});
+
+  
+
+	$('body').on('click', '#test_button_en' , function() {
+
+    languageScript.remove();
+
+    lang = "en";
+
+    appendLanguageScript();
+
+		convertLanguage();
+
+	});
+
+});
+
+
+
+function appendLanguageScript()
+
+{
+
+  languageScript = $('<script>').attr({
+
+    'type': 'text/javascript',
+
+    'src': 'https://mentholatumdryeye.github.io/test.io/language/'+lang+'.js'
+
+  });
+
+  $('body').append(languageScript);
+
+}
+
+
+
+function convertLanguage()
+
+{
+
+  for (let key in languageList) {
+
+    $('.'+key).text(languageList[key]);
+
+  }
+
 }
